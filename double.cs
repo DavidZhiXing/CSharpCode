60b005aeaea5ab4502ff1c1594e464f179025a26
Xing <wow_only@163.com>
2021-12-17 14:11:13 +0800

    test remote

--- double.cs
+++ double.cs
@@ -7,3 +7,19 @@ public class double_to_string {
     }
 }
 
+// md5 endcode
+
+public static string MD5(string str)
+{
+    MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+    byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);
+    bytes = md5.ComputeHash(bytes);
+    md5.Clear();
+    string ret = "";
+    for (int i = 0; i < bytes.Length; i++)
+    {
+        ret += Convert.ToString(bytes[i], 16).PadLeft(2, '0');
+    }
+    return ret.PadLeft(32, '0');
+}
+
